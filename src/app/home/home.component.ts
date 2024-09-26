43e0b3c67e1522c755c59d1089a8bfeb844c7e17
Nishant Krishna Ghadigaonkar <nishantkrishna1288@gmail.com>
2024-09-26 06:18:06 +0530

    functionality of adding and removing qualification

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
- show : boolean = true;
+  show: boolean = true;
+
+  // decalre variable for 2 way data binding
+  name = '';
+  email = '';
+  phone = '';
+  address = '';
+
+  // array for qualification. Needed because we need to render multipe fields for it.
+  qualification = [{
+    school: '',
+    degree: '',
+    year: ''
+  }]
+
+  //function to add qualification of user
+  addQualification() {
+    this.qualification.push({ school: '', degree: '', year: '' })
+  }
+
+  deleteQualification(indexNumber : number) {
+    this.qualification.splice(indexNumber, 1)
+  }
+
 }
